b3ff64f489744b26f70dcf860642f66a8a4c935b
Edinson Florez <97996733+dstx7@users.noreply.github.com>
2022-10-25 09:04:59 -0500

    c2

--- proyecto_back/app.js
+++ proyecto_back/app.js
@@ -4,12 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var database = require("./config/database");
+var auth = require("./auth/main_auth")
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var empleadosRouter = require('./routes/empleados.router');
 var noviosRouter = require('./routes/novios.router')
+var usuariosRouter = require('./routes/usuarios.router')
+
+
 
 var app = express();
 
@@ -21,12 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //mongo connection 
 database.mongoConnect();
-
+app.use('/usuarios', usuariosRouter)
+app.use(auth)
 
 //route 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/empleados', empleadosRouter);
 app.use('/novios', noviosRouter);
 
